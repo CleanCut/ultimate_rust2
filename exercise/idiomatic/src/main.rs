53909a5433664db249d5ffbcba5fdbee9defe4d2
Nathan Stocks <cleancut@github.com>
2021-08-16 21:48:08 -0600

    Add note about auto-running cargo fmt upon save

--- exercise/idiomatic/src/main.rs
+++ exercise/idiomatic/src/main.rs
@@ -1,4 +1,5 @@
-// 1. This code looks terrible. Let's start cleaning this up by running `cargo fmt`.
+// 1. This code looks terrible. Let's start cleaning this up by running `cargo fmt`. If you
+// configured your editor or IDE to run `cargo fmt` automatically upon save, you can just save!
 
 // 2. `cargo fmt` is great, but it doesn't add blank lines where there are none. Go ahead and add
 // some blank lines in places you think it would make sense.
